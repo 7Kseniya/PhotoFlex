7451926be7f568a87d020d8d5cb5c8261fdc0936
ferea <tatamitina19@gmail.com>
2024-10-11 14:54:30 +0300

    g3 unit test coverage < 80% failed

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import React, { useState } from 'react';
 
 function App() {
   const [click, setClick] = useState(false);
+  const [anotherClick, setAnotherClick] = useState(false);
 
   const handleClick = () => {
     setClick((prevClick) => !prevClick);
   };
+
+  const handleAnotherClick = () => {
+    setAnotherClick((prevClick) => !prevClick);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <button onClick={handleClick}>
           {click ? 'Ура' : 'Нажми на меня'}
         </button>
+        <button onClick={handleAnotherClick}>
+          {' '}
+          {anotherClick ? 'Еще' : 'Нажми еще раз'}
+          Нажми еще раз
+        </button>{' '}
       </header>
     </div>
   );
