54a665189f8ef6201e216da377f782f43aecb816
ferea <tatamitina19@gmail.com>
2024-10-11 14:59:28 +0300

    g3 e2e test failed

--- cypress/integration/cypTest1.spec.js
+++ cypress/integration/cypTest1.spec.js
@@ -6,7 +6,7 @@ describe('App Component', () => {
   it('should change button text on click', () => {
     cy.get('button').should('contain', 'Нажми на меня');
     cy.get('button').click();
-    cy.get('button').should('contain', 'Ура');
+    cy.get('button').should('contain', 'Не ура');
   });
   it('should have a visible button', () => {
     cy.get('button').should('be.visible');
