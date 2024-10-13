36f4f2c38c589d37a36a7fb543ea1cbdc31f337e
liza <368051@edu.itmo.ru>
2024-10-13 19:29:19 +0300

    added image-rotate test

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,10 +1,31 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen, waitFor} from '@testing-library/react';
 import React from 'react';
 import '@testing-library/jest-dom';
 import App from './../src/components/app/app';
-
+import ImageRotate from './../src/components/editor-actions/image-rotate';
 test('renders MainPage component', () => {
   render(<App />);
   const mainPageElement = screen.getByTestId('main-page');
   expect(mainPageElement).toBeInTheDocument();
+});
+
+
+test('ImageRotate Component correctly sets canvas dimensions based on rotation', async () => {
+  global.Image = class {
+    constructor() {
+      setTimeout(() => {
+        if (this.onload) this.onload();
+      }, 0);
+    }
+  };
+
+  const { container } = render(
+      <ImageRotate imageSrc="test-image.jpg" rotation={45} />
+  );
+  const canvas = container.querySelector('canvas');
+
+  await waitFor(() => {
+    expect(canvas.width).toBeGreaterThan(0);
+    expect(canvas.height).toBeGreaterThan(0);
+  });
 });
