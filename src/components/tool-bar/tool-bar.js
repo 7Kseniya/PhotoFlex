392a72fb0d37c901b9e0df64d548ee3266cb121d
Irina Khvostova <368991@niuitmo.ru>
2024-10-13 16:50:41 +0300

    TuneIcon done

--- src/components/tool-bar/tool-bar.js
+++ src/components/tool-bar/tool-bar.js
@@ -1,7 +1,12 @@
 import styles from './tool-bar.module.css';
 import React from 'react';
+import TuneIcon from '@mui/icons-material/Tune';
 
 const ToolBar = () => {
-  return <div className={styles.mainContainer}></div>;
+  return (
+    <div className={styles.mainContainer}>
+      <TuneIcon className={`${styles.icon} ${styles.tune}`} />
+    </div>
+  );
 };
 export default ToolBar;
