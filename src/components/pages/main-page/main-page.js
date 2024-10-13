cd0cd883f56146eaca24f7eb0b8ccef9af74233e
liza <368051@edu.itmo.ru>
2024-10-13 18:49:26 +0300

    added image-rotate to main paige

--- src/components/pages/main-page/main-page.js
+++ src/components/pages/main-page/main-page.js
@@ -2,17 +2,24 @@ import styles from './main-page.module.css';
 import Header from '../../header/header';
 import ToolBar from '../../tool-bar/tool-bar';
 import Tools from '../../tools/tools';
-import UploadContainer from '../../upload-container/upload-container';
-import React from 'react';
+import ImageRotate from '../../editor-actions/image-rotate';
+import React, { useState } from 'react';
 
 const MainPage = () => {
+  const [imageSrc, setImageSrc] = useState('/placeholder.jpeg');
+  const [rotation, setRotation] = useState(0);
+
+  const handleRotate = () => {
+    setRotation((prevRotation) => prevRotation + 90);
+  };
+
   return (
     <div className={styles.mainContainer} data-testid="main-page">
       <Header />
       <div className={styles.toolContainer}>
-        <ToolBar />
+        <ToolBar onRotate={handleRotate} />
         <Tools />
-        <UploadContainer />
+        <ImageRotate imageSrc={imageSrc} rotation={rotation} />
       </div>
     </div>
   );
