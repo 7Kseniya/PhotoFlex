c4c107f8c267924a81a09c9b460656f273c77c9e
ferea <tatamitina19@gmail.com>
2024-10-15 00:20:04 +0300

    g3.4 unit tests failed

--- tests/first.test.js
+++ tests/first.test.js
@@ -14,7 +14,7 @@ test('renders button and toggles text on click', () => {
 });
 
 test('multiplies 2 * 3 to equal 6', () => {
-  expect(2 * 3).toBe(6);
+  expect(2 * 3).toBe(7);
 });
 test('subtracts 5 - 3 to equal 2', () => {
   expect(5 - 3).toBe(2);
