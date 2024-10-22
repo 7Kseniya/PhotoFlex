7b0ba5aa1b01b2b8a145d12691b43979ebfae749
Irina Khvostova <368991@niuitmo.ru>
2024-10-22 22:44:46 +0300

    added blocks for filters

--- src/components/tools/tools.js
+++ src/components/tools/tools.js
@@ -2,6 +2,25 @@ import styles from './tools.module.css';
 import React from 'react';
 
 const Tools = () => {
-  return <div className={styles.mainContainer}></div>;
+  const filters = [
+    { name: 'nebula' },
+    { name: 'outerspace' },
+    { name: 'refulgence' },
+    { name: 'grayscale' },
+    { name: 'grayscale' },
+    { name: 'grayscale' },
+  ];
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.filterContainer}>
+        {filters.map((filter, index) => (
+          <div key={index} className={styles.filterItem}>
+            <div className={styles.filterBlock}></div>
+            <p className={styles.filterLabel}>{filter.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Tools;
