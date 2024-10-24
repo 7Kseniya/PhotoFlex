013e98df3a7f558a8ee4e2af4db8e11794e1107f
Irina Khvostova <368991@niuitmo.ru>
2024-10-24 23:06:37 +0300

    [ADD] editing username

--- src/components/personal-account/personal-account.js
+++ src/components/personal-account/personal-account.js
@@ -1,13 +1,21 @@
 import styles from './personal-account.module.css';
 import logoImg from '../../images/logo.png';
 import EditIcon from '@mui/icons-material/Edit';
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const PersonalAccount = () => {
   const navigate = useNavigate();
   const goToHome = () => {
     navigate('/');
+  };
+  const [userName, setUserName] = useState('Username');
+  const [isEditing, setIsEditing] = useState(false);
+  const handleEditClick = () => {
+    setIsEditing(!isEditing);
+  };
+  const handleNameChange = (event) => {
+    setUserName(event.target.value);
   };
   return (
     <div className={styles.mainContainer}>
@@ -16,12 +24,33 @@ const PersonalAccount = () => {
           src={logoImg}
           className={styles.logoContainer}
           onClick={goToHome}
-          style={{ cursor: 'pointer' }}
           alt={'Logo'}
         />
-        <EditIcon className={`${styles.icon} ${styles.edit}`} />
+        <EditIcon
+          className={`${styles.icon} ${styles.edit}`}
+          onClick={handleEditClick}
+        />
       </div>
-      <div className={styles.userContainer}></div>
+      <div className={styles.userContainer}>
+        <div className={styles.editSection}>
+          {isEditing ? (
+            <>
+              <input
+                type="text"
+                value={userName}
+                onChange={handleNameChange}
+                className={styles.nameInput}
+              />
+              <EditIcon
+                className={styles.editIcon}
+                onClick={handleEditClick}
+              />
+            </>
+          ) : (
+            <p className={styles.userName}>{userName}</p>
+          )}
+        </div>
+      </div>
       <section className={styles.photosContainer}>
         <div className={styles.photo}></div>
         <div className={styles.photo}></div>
