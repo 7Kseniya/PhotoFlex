e399636e77b7b160cbeabcf77f33e09eaa98a3d8
ferea <tatamitina19@gmail.com>
2024-11-01 01:27:32 +0300

    add CI develop

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,14 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import styles from './modal.module.css';
 import ModalOverlay from '../modal/modal-overlay';
 import ClearIcon from '@mui/icons-material/Clear';
 const modalRoot = document.getElementById('modal-root');
 const Modal = ({ children, onClose }) => {
-  // const [open, setOpen] = useState(false);
-
-  // const handleClose = () => setOpen(false);
-  // const handleOpen = () => setOpen(true);
   useEffect(() => {
     const handleClose = (event) => {
       if (event.key === 'Escape') {
