58367288413ee8a1b8c2d6a31f8b45e078826e31
ferea <tatamitina19@gmail.com>
2024-11-11 18:18:15 +0300

    Updates

--- cypress/integration/cypTest1.spec.js
+++
@@ -1,6 +0,0 @@
-describe('service is available', function () {
-  // eslint-disable-next-line jest/expect-expect
-  it('should be available on localhost:3000', function () {
-    cy.visit('http://localhost:3000');
-  });
-});
