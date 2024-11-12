336d07449d5917b92745702df29e578dc266adb4
liza <368051@edu.itmo.ru>
2024-11-12 13:20:48 +0300

    Updates

--- src/components/modal/modal-overlay.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from '../modal/modal-overlay.module.css';
-
-const ModalOverlay = ({ onClose }) => {
-  return <div className={styles.overlayBack} onClick={onClose}></div>;
-};
-
-export default ModalOverlay;
