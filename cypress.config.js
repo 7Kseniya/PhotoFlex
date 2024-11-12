fc6420b752e8f6fcc8c61473a7f2aa719597ec1a
liza <368051@edu.itmo.ru>
2024-11-12 16:51:37 +0300

    cypress fixed

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
     },
   },
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:3000',
     specPattern: 'cypress/integration/*.spec.js',
   },
 });
