26757c573d1b87a2377e6ef26b2dc6d539fafdb3
Ксения Игнатьева <kseniaignateva@MacBook-Pro-Ksenia.local>
2024-11-30 21:51:43 +0300

    modify modal overlay component

--- src/components/modal/modal-overlay.js
+++ src/components/modal/modal-overlay.js
@@ -2,7 +2,13 @@ import React from 'react';
 import styles from '../modal/modal-overlay.module.css';
 
 const ModalOverlay = ({ onClose }) => {
-  return <div className={styles.overlayBack} onClick={onClose}></div>;
+  return (
+    <div
+      className={styles.overlayBack}
+      onClick={onClose}
+      data-testid="modal-overlay"
+    ></div>
+  );
 };
 
 export default ModalOverlay;
